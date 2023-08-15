d03ab3de29d334516c5300f35d270ab7698abda5
SRE <aero.safety.brian@gmail.com>
2023-08-15 16:39:02 -0500

    rename methods to nor... south...

--- powershell-azure-function-helloworldHttpTrigger/run.ps1
+++
@@ -1,6 +0,0 @@
-param($Request)
-
-Push-OutputBinding -Name Response -Value (@{
-        StatusCode = "ok"
-        Body       = "From a PowerShell Azure Function - Hello World!"
-    })
---
+++ southDCdatabaseSim/run.ps1
@@ -0,0 +1,6 @@
+param($Request)
+
+Push-OutputBinding -Name Response -Value (@{
+    StatusCode = "ok"
+    Body = "From a PowerShell Azure Function - Hello World!"
+})
